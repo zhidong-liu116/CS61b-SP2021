c360e28dbee47b61b26e4c355bcf9411a0a32869
Zhidong <zliu2355@gmail.com>
2024-10-04 18:30:30 -0400

    completed first part of lab1

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -3,9 +3,9 @@ public class HelloNumbers {
         int x = 1;
         int total = 0;
         while (x <= 10) {
-            System.out.print(total + " ");
-            total = total + x;
-            x = x + 1;
+            System.out.print(total + " "); // 0, 1, 3, 6, 10
+            total = total + x; // 1, 1+2=3, 3+3=6, 6+4=10, 15
+            x = x + 1; // 2, 2+1, 3+1,4+1
         }
 	}
 } 
