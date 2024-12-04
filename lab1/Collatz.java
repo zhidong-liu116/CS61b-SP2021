6e9eb68fd3a8e39a1db0932f81d489a06b9c1e8a
Eric-Zdl <64428503+zhidong-liu116@users.noreply.github.com>
2024-12-03 20:49:30 -0500

    Restored original Collatz implementation from lab1commit

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,18 +2,20 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
-    /** Correct implementation of nextNumber! */
-    public static int nextNumber(int n) {
-        if (n == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
-    }
 
-    public static void main(String[] args) {
+    /** Buggy implementation of nextNumber! */
+    // Done
+    public static int nextNumber(int n) {
+        if (n % 2 == 0) {
+            n = n / 2;
+        } else if (n % 2 == 1) {
+            n = 3 * n + 1;
+        } else {
+            return 1;
+        }
+        return n;
+    }
+        public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
         while (n != 1) {
@@ -23,4 +25,5 @@ public class Collatz {
         System.out.println();
     }
 }
+
 
