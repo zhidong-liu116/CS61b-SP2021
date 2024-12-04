b0333b5b4a2321a9f903fc001283f529f055dd98
Eric-Zdl <64428503+zhidong-liu116@users.noreply.github.com>
2024-12-03 19:26:37 -0500

    Replaced nextNumber with buggy implementation for conflict testing

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,16 +4,14 @@
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
-    // Done
     public static int nextNumber(int n) {
-        if (n % 2 == 0) {
-            n = n / 2;
-        } else if (n % 2 == 1) {
-            n = 3 * n + 1;
-        } else {
+        if (n  == 128) {
             return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
         }
-        return n;
     }
         public static void main(String[] args) {
         int n = 5;
